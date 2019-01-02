57eb86a1bf7557ba20de3446126a05617f9120e1
abdgonivich <iida.toru19920227@gmail.com>
2019-01-02 12:01:52 +0900

    first commit

---
+++ src/main/java/toru/iida/sample/App.java
@@ -0,0 +1,16 @@
+package toru.iida.sample;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+    	ObjectMapper a;
+        System.out.println( "Hello World!" );
+    }
+}
